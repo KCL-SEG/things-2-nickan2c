5d13a53d62f0b2283e30de7dd1c138491f6e0725
Nickan Tousi <nickan.tousi@kcl.ac.uk>
2022-11-30 13:07:00 +0000

    Exercise 1 and 2

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,24 @@
 """Forms of the project."""
 
 # Create your forms here.
+
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Thing
+
+class ThingForm(forms.ModelForm):
+#     """Form of the project."""
+
+    class Meta:
+        """Meta class of the form."""
+        model = Thing
+        # The description field must be displayed as a Textarea. The quantity field must be displayed as NumberInput.
+
+        fields = ['name', 'description', 'quantity']
+
+        widgets = {
+            'description': forms.Textarea,
+            'quantity': forms.NumberInput
+        }
+
+
